cd76b4e4b59dd4263a0fdce5a2de027fbdfef53b
mohamed afifi <mohamed@afifi.com>
2023-07-18 15:14:13 -0700

    first task

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,17 @@
-#include <main.h>
+#include "main.h"
 /**
  * main - print _putchar
- * is written by mohamed
- * Return: always 0 (success)
+ * Return: 0
  */
 
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+	char w[] = "_putchar";
+
+	for (int i = 0; i < 8; i++)
+	{
+		_putchar (w[i]);
+	}
+	_putchar ('/n');
+	return (0);
 }
