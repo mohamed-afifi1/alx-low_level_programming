15257a1794ac46cc4aaab2bd657b4709d3addbd2
root <root@DESKTOP-HU468P5.localdomain>
2023-10-11 04:43:35 +0300

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,3 +1,4 @@
+
 #ifndef DOG_H
 #define DOG_H
 
@@ -9,12 +10,17 @@
  *
  * Description: Longer description
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
