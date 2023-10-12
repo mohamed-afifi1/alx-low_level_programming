2b292acc4753ca489ccd349dd5ce305ffe5e4837
root <root@DESKTOP-HU468P5.localdomain>
2023-10-12 03:19:22 +0300

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+if (array && size && cmp)
+{
+for (int i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+return (i);
+}
+}
+return (-1);
+}
